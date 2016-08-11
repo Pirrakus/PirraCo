ef8c645359155cea529b565fe4b94a9594abda03
Pirrakus <nestorbarril@hotmail.com>
2016-08-12 01:22:45 +0200

    Update main.swift

--- Codigo Server/main.swift
+++ Codigo Server/main.swift
@@ -1,10 +1,10 @@
-//
-//  main.swift
-//  Codigo Server
-//
-//  Created by Néstor Barril del Valle on 12/8/16.
-//  Copyright © 2016 Néstor Barril del Valle. All rights reserved.
-//
+/*-------------------------------------------------------------------|
+|  main.swift                                                        |
+|  Codigo Server                                                     |
+|                                                                    |
+|  Created by Néstor Barril del Valle on 12/8/16.                    |
+| Copyright © 2016 Néstor Barril del Valle. All rights reserved.     |
+|-------------------------------------------------------------------*/
 
 import Foundation
 
